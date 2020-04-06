ea672f2c9a228980a4284769f1a618bc9f1eefb0
razgino11 <52932887+razgino11@users.noreply.github.com>
2020-04-06 11:36:58 +0300

    changed the name of the main function to be able to be imported and added npm install to install the relavant node pacakages before building

--- buildComboWebClient.py
+++ buildComboWebClient.py
@@ -1,10 +1,14 @@
 import os
 
-def main():
+def buildComboWebClient():
     try:
         if os.system('ng --version') != 0:
             raise Exception('Angular is not installed on this computer')
 
+        if os.system('npm -v') != 0:
+            raise Exception('Npm is not installed on this computer')
+
+        os.system('npm install')
         os.system('ng build')
     except:
         print("Something went wrong, fetching angular's version failed")
@@ -12,4 +16,4 @@ def main():
     print ("Build finished")
 
 if (__name__ == '__main__'):
-    main()
+    buildComboWebClient()
